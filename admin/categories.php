a121425d4df8e091f4f0681507a176b08fed1f12
gushindev <gushindev@gmail.com>
2022-06-19 13:49:30 +0300

    Add delete category button with logic

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,16 @@
 <?php include('admin-includes/header.php'); ?>
 
+<?php
+if (isset($_GET['delete_category'])) {
+  $cat_id = $_GET['delete_category'];
+  $sql = "DELETE FROM `categories` WHERE `cat_id` = '$cat_id'";
+  $result = mysqli_query($connection, $sql);
+  if (!$result) {
+    die(mysqli_error($connection));
+  }
+  header('Location: ./categories.php');
+} ?>
+
 <div id="wrapper">
   <?php include('admin-includes/navigation.php'); ?>
 
@@ -14,19 +25,18 @@
           </h1>
           <div class="col-xs-4">
             <?php
-
             if (isset($_POST['submit'])) {
 
-              if (isset($_POST['new-category']) && $_POST['new-category'] !== '') {
+              if (empty($_POST['new-category']) || ($_POST['new-category'] === '')) {
+                echo ('<p>empty field</p>');
+              } else {
                 $new_category = $_POST['new-category'];
                 $sql = "INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES (NULL, '{$new_category}');";
                 $result = mysqli_query($connection, $sql);
                 if (!$result) {
-                  die('GG');
+                  die('Something went wrong. Please try again later.');
                 }
                 header('Location: ./categories.php');
-              } else {
-              echo '<p>empty field</p>';
               }
             }
             ?>
@@ -37,6 +47,16 @@
               </div>
               <div class="form-group">
                 <button type="submit" class="btn btn-primary" name="submit">Add</button>
+              </div>
+            </form>
+            <p>Edit category</p>
+
+            <form action="categories.php" method="post" class="form">
+              <div class="form-group">
+                <input type="text" name="new-category" id="new-category" class="form-control">
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="submit">Edit</button>
               </div>
             </form>
           </div>
@@ -58,9 +78,8 @@
                   <tr>
                     <td scope='col'><?= $row['cat_id'] ?></td>
                     <td scope='col'><?= $row['cat_title'] ?></td>
-                    <td scope='col'><a href="">Update</a></td>
-                    <td scope='col'><a href="">Delete</a></td>
-
+                    <td scope='col'><a href="categories.php?edit_category=<?= $row['cat_id'] ?>">Edit</a></td>
+                    <td scope='col'><a href="categories.php?delete_category=<?= $row['cat_id'] ?>">Delete</a></td>
                   </tr>
                 <?php endwhile; ?>
               </tbody>

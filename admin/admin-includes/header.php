0c5ff0a1b6a10ae99cf751e2461c03b9ebc205e8
gushindev <gushindev@gmail.com>
2022-06-22 13:53:44 +0300

    Change title of main pages

--- admin/admin-includes/header.php
+++ admin/admin-includes/header.php
@@ -13,7 +13,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Bootstrap Admin Template</title>
+  <title>Admin page</title>
 
   <!-- Bootstrap Core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
